f940354c27bfa95527ab2940d9da6f143d1436f9
AbuCoder <abubakrabdujalilov09@gmail.com>
2025-03-24 18:36:11 +0300

    Errors fixed

--- config/database.js
+++ config/database.js
@@ -1,22 +1,21 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
 const db = new Sequelize({
-  dialect: "mysql", 
-  host: "localhost",  
-  username: "root",   
-  password: "12345678",   
-  database: "db",  
+  dialect: "mysql",
+  host: "localhost",
+  username: "root",
+  password: "12345678",
+  database: "db",
 });
 
 async function connect() {
   try {
     await db.authenticate();
     console.log("Connection has been established successfully.");
-    db.sync({force: true});
-    console.log("Database synced.");  
+    db.sync({ force: true });
+    console.log("Database synced.");
   } catch (error) {
     console.log("Unable to connect to the database:", error);
-    
   }
 }
 
--- main.js
+++ main.js
@@ -1,19 +1,11 @@
-const express = require('express')
-const {connect} = require("./config/database")
-const app = express(); app.use(express.json())
-connect()
+const express = require("express");
+const { connect } = require("./config/database");
+const app = express();
+app.use(express.json());
+connect();
 
 app.get("/", (req, res) => {
-    res.send("Hello World")
-})
+  res.send("Hello World");
+});
 
-
-
-
-
-
-app.listen(3006, ()=>console.log("started on port 3006"))
-
-
-
-
+app.listen(3006, () => console.log("started on port 3006"));
