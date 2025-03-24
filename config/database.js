ec46971808074d0ce6dc46c784a7abffa4436168
AbuCoder <abubakrabdujalilov09@gmail.com>
2025-03-24 16:41:10 +0300

    file va papkalar qoshildi

---
+++ config/database.js
@@ -0,0 +1,15 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+const db = new Sequelize("root", "12345678", "db", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+async function connect() {
+  await db.authenticate();
+  console.log("Connection has been established successfully.");
+  // db.sync({force: true});
+  console.log("Database synced.");
+}
+
+module.exports = { db, DataTypes, connect };
